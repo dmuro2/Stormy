7bbb1f427e44a2fc7d95b25057fc3640bb556148
David Muro II <dmuro2@gmail.com>
2015-01-19 21:54:58 -0800

    Create a Struct

---
+++ Stormy/Stormy/Current.swift
@@ -0,0 +1,13 @@
+//
+//  Current.swift
+//  Stormy
+//
+//  Created by David Muro II on 1/19/15.
+//  Copyright (c) 2015 David Muro II. All rights reserved.
+//
+
+import Foundation
+
+struct Current {
+    //properties and methods
+}
--- Stormy/Stormy/ViewController.swift
+++ Stormy/Stormy/ViewController.swift
@@ -32,7 +32,9 @@ class ViewController: UIViewController {
                 let dataObject = NSData(contentsOfURL: location)
                 let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as NSDictionary
                 
-                println(weatherDictionary)
+                
+                // shouldn't use a black box solution like this, because we can't see what data it contains and we can't easily update code if there are changes to the API. Best to create a Struct in this instance.
+//                let currentWeatherDictionary: NSDictionary = weatherDictionary["currently"] as NSDictionary
                 
             }
             
